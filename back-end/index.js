09950a5597d0d362fb9595ddae0277dfa576bc01
gimseonjin <kimseonjin616@naver.com>
2021-03-05 17:22:53 +0900

    mongodb test
    mongodb test

--- back-end/index.js
+++ back-end/index.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const app = express();
-  
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-  
+
+var test1;
+
+const MongoClient = require('mongoose')
+MongoClient.connect("mongodb+srv://admin:emm05235@cluster0.umzeh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
+    { useNewUrlParser: true, useUnifiedTopology: true, dbName:"sample_analytics" }).then(() => console.log('mongodb connected...')).catch(err => console.log(err))
+
+var Schema = MongoClient.Schema;
+
+var accoundSchema = new Schema(
+    {id: String, account_id: String, limit: String, prodcts : String}
+)
+
+var datas = MongoClient.model('test', accoundSchema, 'accounts')
+
+datas.find(function(error, test){
+    console.log('--- Read all ---');
+    if(error){
+        console.log(error);
+    }else{
+        console.log(test);
+        test1=test
+    }
+})
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   
   console.log('Server is working : PORT - ',port);
 });
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World!'+test1);
+  });
+  
