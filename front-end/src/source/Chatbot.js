42ccd17dd1efcdeeefb8d93832bac90d37d901b5
gimseonjin <kimseonjin616@naver.com>
2021-05-23 11:59:20 +0900

    add css ver 3

--- front-end/src/source/Chatbot.js
+++ front-end/src/source/Chatbot.js
@@ -1,9 +1,10 @@
 import Chatbot from "react-chatbot-kit"
-import chatbot2 from '../Image/help.png'
+import chatbot2 from '../Image/helpEx.png'
+import cancel from '../Image/loupe.png'
 import MessageParser from '../chatbot/MessageParser';
 import ActionProvider from '../chatbot/ActionProvider';
 import config from '../chatbot/config';
-
+import './css/Menubar.css'
 import './css/Sidebar.css'
 
 const HappyChatbot = (props) => {
@@ -25,6 +26,10 @@ const HappyChatbot = (props) => {
           config={config} 
           actionProvider={ActionProvider} 
           messageParser={MessageParser}/>
+        </div>
+        <div id = "chatbotM">
+            <div id = "messageText1"><img src={cancel} id="cancelImg"/>청년 행복주택 알리미 </div>
+            <div id = "messageText2">무엇을 도와드릴까요?</div>
         </div>
         <img alt="chatbot" src={chatbot2} className="chatbot-button" onClick={() => chatbotFAQ()}/>
 
--- front-end/src/source/MapMarkers.js
+++ front-end/src/source/MapMarkers.js
@@ -3,14 +3,24 @@ import axios from 'axios';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react'
 import numeral from 'numeral'
 
-import image from '../Image/apartment_.png'
 import SearchBar from './SearchBar'
 import SideBar from './SideBar';
 import './css/SearchBar.css';
 
+import image from '../Image/placeholder.png'
+import cancel from '../Image/loupe.png'
+import undo from '../Image/undo.png'
+import like1 from '../Image/like.png'
+import like2 from '../Image/like-toggle.png'
+import room4 from '../Image/room4.PNG'
+import chatbot2 from '../Image/helpEx.png'
+import star from '../Image/star.png'
+
+
 export const MapMarkers = (props) => { 
     const [item, setItem] = useState([]);
     const [houseDetail, setHouseDetail] = useState({
+      _id:"",
       danjiCode: "",
       danjiName: "",
       address: "",
@@ -58,16 +68,16 @@ export const MapMarkers = (props) => {
           position={{lat:data.lat, lng:data.lng}}
           icon={{
             url: image,
-            scaledSize: new props.google.maps.Size(40,40),
+            scaledSize: new props.google.maps.Size(30,30),
             //labelOrigin: new props.google.maps.Size(50, 115),
           }}
-          label={{
-            text: `${numeral(data.houseHoldNum).format('0,0')}세대`,
-            fontSize: "13px",
-            fontFamily: "Do Hyeon",
-            color: "white",
-            className: 'label'
-          }}
+          // label={{
+          //   text: `${numeral(data.houseHoldNum).format('0,0')}세대`,
+          //   fontSize: "13px",
+          //   fontFamily: "Do Hyeon",
+          //   color: "white",
+          //   className: 'label'
+          // }}
           onClick = {()=>{
             sidebarShow();
           setHouseDetail(data);}}
